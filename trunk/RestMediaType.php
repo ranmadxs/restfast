dabeb80d49c5284e287ebf0469b96616d9ceb845
grineldosanchez@gmail.com <grineldosanchez@gmail.com@96e9299d-f77c-b40d-d800-06c41135bcd6>
2013-05-30 19:07:40 +0000

    Inicio proyecto

---
+++ trunk/RestMediaType.php
@@ -0,0 +1,14 @@
+<?php
+
+class RestMediaType{
+    const TYPE_JSON = "json";
+    const TYPE_HTML = "html";
+}
+
+class RestRequestType{
+    const TYPE_GET = "GET";
+    const TYPE_POST = "POST";
+    const TYPE_DELETE = "DELETE";
+}
+
+?>
